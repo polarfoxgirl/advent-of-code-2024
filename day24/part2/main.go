035f9c6482d43e876f2d346b6deaddd8a2b8a744
Olga Nikonova <olgan@spotify.com>
2025-01-15 02:03:06 -0500

    Solve day 24 part 2

--- day24/part2/main.go
+++ day24/part2/main.go
@@ -25,73 +25,172 @@ type gate struct {
 }
 
 func main() {
-	data, err := os.ReadFile("input.txt")
+	data, err := os.ReadFile("input_fixed.txt")
 	check(err)
 
 	values, gates := parseInput(data)
 	fmt.Printf("Input: %d inputs, %d gates\n", len(values), len(gates))
 
+	inputAnds := make(map[string]string)
+	inputXors := make(map[string]string)
+	for _, gate := range gates {
+		if strings.HasPrefix(gate.left, "x") || strings.HasPrefix(gate.left, "y") {
+			code := gate.left[1:]
+			if gate.op == AND {
+				inputAnds[code] = gate.out
+			} else if gate.op == XOR {
+				inputXors[code] = gate.out
+			} else {
+				panic(fmt.Sprintf("Invalid input gate: %v", gate))
+			}
+		}
+	}
+
+	// 45 inputs from x00 to x44
+	if len(inputAnds) != 45 || len(inputAnds) != len(inputXors) {
+		fmt.Printf("Incomplete input ANDs (%d) or input XORs (%d)\n", len(inputAnds), len(inputXors))
+	}
+
+	if z00Out, ok := inputXors["00"]; !ok || z00Out != "z00" {
+		fmt.Println("Missing z00 gate")
+	}
+
+	orOuts := make(map[string]string)
+	andOuts := make(map[string]string)
+	for code, inputXor := range inputXors {
+		// x00 AND y00 -> hmc
+		// x00 XOR y00 -> z00
+
+		if code != "00" {
+			for _, gate := range gates {
+				if gate.left == inputXor || gate.right == inputXor {
+					if gate.op == XOR {
+						if strings.HasPrefix(gate.out, "z") {
+							if gate.out[1:] != code {
+								fmt.Printf("Invalid z gate %v (for code %s and input %s)\n", gate, code, inputXor)
+							}
+
+							prevCode := fmt.Sprintf("%d", parseInt(code)-1)
+							if len(prevCode) == 1 {
+								prevCode = "0" + prevCode
+							}
+							if gate.left == inputXor {
+								orOuts[prevCode] = gate.right
+							} else {
+								orOuts[prevCode] = gate.left
+							}
+						} else {
+							// Gate {nfh nqq 2 qgd} should be a z-gate for code 18
+							fmt.Printf("Gate %v should be a z-gate for code %s\n", gate, code)
+						}
+					} else if gate.op == AND {
+						if strings.HasPrefix(gate.out, "z") {
+							// Invalid shift AND gate {cvt wwp 0 z33} (for code 33 and input wwp)
+							// Swap: z33 and ?gqp
+							// wwp XOR cvt -> gqp
+							// cvt AND wwp -> z33
+							fmt.Printf("Invalid shift AND gate %v (for code %s and input %s)\n", gate, code, inputXor)
+						} else {
+							andOuts[code] = gate.out
+						}
+					} else {
+						// Invalid OR gate {bmv hsw 1 mtn} (for code 24 and input hsw)
+						// Swap: hsw and ?jmh
+						// y24 XOR x24 -> hsw
+						// y24 AND x24 -> jmh
+						// jmh AND mrs -> bmv
+						// jmh XOR mrs -> z24
+						fmt.Printf("Invalid OR gate %v (for code %s and input %s)\n", gate, code, inputXor)
+					}
+				}
+			}
+		}
+	}
+
+	for code, inputAnd := range inputAnds {
+		// Technically need to validate z00 transfer, but it's fine
+		if code != "00" {
+			if andOut, ok := andOuts[code]; ok {
+				for _, gate := range gates {
+					if gate.left == inputAnd || gate.right == inputAnd {
+						if gate.left != andOut && gate.right != andOut {
+							fmt.Printf("Gate %v has inputAnd %s but not andOut %s for code %s\n", gate, inputAnd, andOut, code)
+						}
+
+						if gate.op != OR {
+							fmt.Printf("Non-OR gate %v for code %s\n", gate, code)
+						}
+
+						// Gate {kgd kqf 1 z10} doesn't have expected out mwk for code 10
+						// Swap: z10 and ?mwk
+						// y10 XOR x10 -> sqr
+						// mwq AND sqr -> kgd
+						// sqr XOR mwq -> mwk
+						//
+						// Gate {ndb qqc 1 nfh} doesn't have expected out  for code 17
+						// x17 XOR y17 -> thq +
+						// x17 AND y17 -> qqc +
+						// mfd AND thq -> ndb +
+						// thq XOR mfd -> z17 +
+						// ndb OR qqc -> nfh ...
+						// nfh XOR nqq -> qgd ?
+						// nfh AND nqq -> ggn +
+						// y18 AND x18 -> z18 ?
+						// qgd OR ggn -> cvr
+						// Swap z18 and qgd
+						if code != "44" {
+							if orOut, known := orOuts[code]; !known || orOut != gate.out {
+								fmt.Printf("Gate %v doesn't have expected out %s for code %s\n", gate, orOut, code)
+							}
+						}
+					}
+				}
+			} else {
+				fmt.Printf("Missing and out for code %s\n", code)
+			}
+		}
+	}
+
+	// Technically need to validate z45 gate, but it's fine
+
 	unprocessed := make(map[string]struct{})
-	for wire, _ := range values {
+	for wire := range values {
 		unprocessed[wire] = struct{}{}
 	}
 	for len(unprocessed) > 0 {
+		usedGates := make([]gate, 0)
 		nextWave := make(map[string]struct{})
 		for _, gate := range gates {
 			if _, knownOut := values[gate.out]; !knownOut {
 				if _, leftUnprocessed := unprocessed[gate.left]; leftUnprocessed {
 					if rightValue, rightPresent := values[gate.right]; rightPresent {
-						values[gate.out] = exec(values[gate.left], rightValue, gate.op)
-						nextWave[gate.out] = struct{}{}
+						if _, inNextWave := nextWave[gate.right]; !inNextWave {
+							values[gate.out] = exec(values[gate.left], rightValue, gate.op)
+							nextWave[gate.out] = struct{}{}
+							usedGates = append(usedGates, gate)
+						}
 					}
 				} else if _, rightUnprocessed := unprocessed[gate.right]; rightUnprocessed {
 					if leftValue, leftPresent := values[gate.left]; leftPresent {
-						values[gate.out] = exec(leftValue, values[gate.right], gate.op)
-						nextWave[gate.out] = struct{}{}
+						if _, inNextWave := nextWave[gate.left]; !inNextWave {
+							values[gate.out] = exec(leftValue, values[gate.right], gate.op)
+							nextWave[gate.out] = struct{}{}
+							usedGates = append(usedGates, gate)
+						}
 					}
 				}
 			}
 		}
 
+		// fmt.Println(usedGates)
 		unprocessed = nextWave
 	}
 
-	// fmt.Printf("Desired: %b\nActual:: %b", getResult(values, "z"), getResult(values, "x")+getResult(values, "y"))
-	inScope := getInScopeGates(gates)
-	fmt.Printf("Only %d gates in scope\n", len(inScope))
+	fmt.Printf("Desired: %b\nActual:: %b\n", getResult(values, "z"), getResult(values, "x")+getResult(values, "y"))
 
-	// Can't do combinations
-
-}
-
-func getInScopeGates(gates []gate) (inScope []gate) {
-	// Desired: 1010010000100110101001011111011111110011101000
-	// Actual:: 1010010000100110101000100000100000000011101000
-	// Only gates upstream of z10 - z24 need to change
-
-	inScope = make([]gate, 0)
-	seen := make(map[string]struct{})
-	queue := make([]string, 0)
-	for n := 10; n < 25; n++ {
-		wire := fmt.Sprintf("z%d", n)
-		queue = append(queue, wire)
-	}
-	for len(queue) > 0 {
-		wire := queue[0]
-		queue = queue[1:]
-
-		if _, ok := seen[wire]; !ok {
-			seen[wire] = struct{}{}
-			for _, gate := range gates {
-				if gate.out == wire {
-					inScope = append(inScope, gate)
-					queue = append(queue, gate.left)
-					queue = append(queue, gate.right)
-				}
-			}
-		}
-	}
-	return
+	answer := []string{"z33", "gqp", "hsw", "jmh", "z10", "mwk", "z18", "qgd"}
+	slices.Sort(answer)
+	fmt.Println(strings.Join(answer, ","))
 }
 
 func exec(left bool, right bool, op operation) bool {
